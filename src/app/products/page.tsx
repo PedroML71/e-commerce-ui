4b4773a7e468419ca3d2cc1ef225c98115c5254c
PedroML71 <pedrolimafsa@hotmail.com>
2025-10-07 13:14:09 -0300

    Add Products Page

---
+++ src/app/products/page.tsx
@@ -0,0 +1,18 @@
+import { FC } from "react";
+import ProductList from "@/components/ProductList";
+
+interface ProductsPageProps {
+  searchParams: Promise<{ category: string }>;
+}
+
+const ProductsPage: FC<ProductsPageProps> = async ({ searchParams }) => {
+  const category = (await searchParams).category;
+
+  return (
+    <div className="">
+      <ProductList category={category} />
+    </div>
+  );
+};
+
+export default ProductsPage;

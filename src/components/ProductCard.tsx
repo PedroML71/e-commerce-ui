54ec6c92d1649704d8774553352e1d107a064cb9
PedroML71 <pedrolimafsa@hotmail.com>
2025-10-06 23:15:49 -0300

    Add Product Image Color Filter

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FC } from "react";
+import { FC, useState } from "react";
 import { ProductType } from "@/types";
 import Link from "next/link";
 import Image from "next/image";
@@ -11,13 +11,31 @@ interface ProductCardProps {
 }
 
 const ProductCard: FC<ProductCardProps> = ({ product }) => {
+  const [productTypes, setProductTypes] = useState({
+    size: product.sizes[0],
+    color: product.colors[0],
+  });
+
+  const handleProductType = ({
+    type,
+    value,
+  }: {
+    type: "size" | "color";
+    value: string;
+  }) => {
+    setProductTypes((prev) => ({
+      ...prev,
+      [type]: value,
+    }));
+  };
+
   return (
     <div className="shadow-lg rounded-lg overflow-hidden">
       {/* IMAGE */}
       <Link href={`/products/${product.id}`}>
         <div className="relative aspect-[2/3]">
           <Image
-            src={product.images[product.colors[0]]}
+            src={product.images[productTypes.color]}
             alt={product.name}
             fill
             className="object-cover hover:scale-105 transition-all duration-300"
@@ -39,6 +57,9 @@ const ProductCard: FC<ProductCardProps> = ({ product }) => {
               name="size"
               id="size"
               className="ring ring-gray-300 rounded-md px-2 py-1"
+              onChange={(e) =>
+                handleProductType({ type: "size", value: e.target.value })
+              }
             >
               {product.sizes.map((size) => (
                 <option value={size} key={size}>
@@ -53,10 +74,20 @@ const ProductCard: FC<ProductCardProps> = ({ product }) => {
             <span className="text-gray-500">Color</span>
             <div className="flex items-center gap-2">
               {product.colors.map((color) => (
-                <div className="" key={color}>
+                <div
+                  className={`cursor-pointer border-1 ${
+                    productTypes.color === color
+                      ? "border-gray-400"
+                      : "border-gray-200"
+                  } rounded-full p-[1.2px]`}
+                  key={color}
+                >
                   <div
                     className="w-[14px] h-[14px] rounded-full"
                     style={{ backgroundColor: color }}
+                    onClick={() =>
+                      handleProductType({ type: "color", value: color })
+                    }
                   />
                 </div>
               ))}

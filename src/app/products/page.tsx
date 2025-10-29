f54ad2c68dc2c0c58db1d48d6a891c48fc80dd32
PedroML71 <pedrolimafsa@hotmail.com>
2025-10-28 23:18:58 -0300

    add api fetching functionality

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,15 +2,22 @@ import { FC } from "react";
 import ProductList from "@/components/ProductList";
 
 interface ProductsPageProps {
-  searchParams: Promise<{ category: string }>;
+  searchParams: Promise<{ category: string; sort: string; search: string }>;
 }
 
 const ProductsPage: FC<ProductsPageProps> = async ({ searchParams }) => {
   const category = (await searchParams).category;
+  const sort = (await searchParams).sort;
+  const search = (await searchParams).search;
 
   return (
     <div className="">
-      <ProductList category={category} params="products" />
+      <ProductList
+        category={category}
+        sort={sort}
+        search={search}
+        params="products"
+      />
     </div>
   );
 };
